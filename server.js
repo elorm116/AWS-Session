7d36e7889bbbfe57451e4559b2b856ec39f8dca9
elorm116 <aezottor@gmail.com>
2025-02-05 15:19:04 +0000

    amended server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const { resolve } = require("path");
 const port = process.env.PORT || 3000;
+const HOST = process.env.HOST || 'localhost';
 
 // importing the dotenv module to use environment variables:
 require("dotenv").config();
@@ -79,5 +80,5 @@ app.post("/create-checkout-session/:pid", async (req, res) => {
 // Server listening:
 app.listen(port, () => {
   console.log(`Server listening on port: ${port}`);
-  console.log(`You may access you app at: ${domainURL}`);
+  console.log(`You may access your app at: http://${HOST}:${port}`);
 });
